7f9a5580353bfb4b2e90db5621e436f7dbb1b36f
M Tahir Khan <mtkhan1@sheffield.ac.uk>
2022-02-26 18:18:46 +0000

    navigation bar and footer 2.0

--- controllers/main-controller.rb
+++ controllers/main-controller.rb
@@ -12,22 +12,23 @@ get "/create_post" do
     erb :create_post
 end
 
-#get "/moderator" do
-#    erb :moderator
-#end
+get "/moderator" do
+    erb :moderator
+end
 
-#get "/admin" do
-#    erb :admin
-#end
+get "/admin" do
+    erb :admin
+end
 
-#get "/viewer" do
-#    erb :viewer
-#end
+get "/viewer" do
+    erb :viewer
+end
 
-#get "/profile" do
-#    erb :profile
-#end
+get "/profile" do
+    erb :profile
+end
 
-#get "/create_account" do
-#    erb :create_account
-#end
+get "/create_account" do
+    erb :create_account
+end
+
