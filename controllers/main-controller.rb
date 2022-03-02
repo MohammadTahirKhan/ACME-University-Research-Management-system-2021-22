7a1d9f86a0fd18fd8103920bdaa35a35fee3dc83
Jude Gibson <jsfgibson1@sheffield.ac.uk>
2022-03-02 11:33:05 +0000

    02.03.22

--- controllers/main-controller.rb
+++ controllers/main-controller.rb
@@ -24,24 +24,6 @@ get "/profile" do
     erb :profile
 end
 
-get "/moderator" do
-    erb :moderator
-end
-
-get "/admin" do
-    erb :admin
-end
-
-get "/viewer" do
-    erb :viewer
-end
-
-get "/profile" do
-    erb :profile
-end
-
 get "/create_account" do
     erb :create_account
 end
-
-
