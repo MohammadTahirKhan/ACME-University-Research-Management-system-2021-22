ff0e6ba9d8aa2e02e10068a8abe76176d6036a47
M Tahir Khan <mtkhan1@sheffield.ac.uk>
2022-03-10 14:46:00 +0000

    login completed

--- controllers/login.rb
+++ controllers/login.rb
@@ -1,34 +1,34 @@
-# get "/" do
-#     redirect "/login" unless session[:logged_in]
-#     erb :home
-#   end
+get "/" do
+    redirect "/login" unless session[:logged_in]
+    erb :home
+  end
   
-#   get "/login" do
-#     @users = User.new
-#     erb :login
-#   end
+  get "/login" do
+    @users = User.new
+    erb :login
+  end
   
-#   post "/login" do
-#     @users = User.new
-#     @users.load(params)
-#     @error = nil
+  post "/login" do
+    @users = User.new
+    @users.load(params)
+    @error = nil
   
-#     if @users.valid?
-#       if @users.exist?
-#         session[:logged_in] = true
-#         redirect "/"
-#       else
-#         @error = "Username/Password combination incorrect"
-#       end
-#     else
-#       @error = "Please correct the information below"
-#     end
+    if @users.valid?
+      if @users.exist?
+        session[:logged_in] = true
+        redirect "/"
+      else
+        @error = "Username/Password combination incorrect"
+      end
+    else
+      @error = "Please correct the information below"
+    end
   
-#     erb :login
-#   end
+    erb :login
+  end
   
-#   get "/logout" do
-#     session.clear
-#     erb :logout
-#   end
+  get "/logout" do
+    session.clear
+    erb :logout
+  end
   
--- models/user.rb
+++ models/user.rb
@@ -29,4 +29,14 @@ class User < Sequel::Model
     self.password =  params.fetch("password", "").strip
   end
 
+  def validate
+    super
+    errors.add("username", "cannot be empty") if username.empty?
+    errors.add("password", "cannot be empty") if password.empty?
+  end
+
+  def exist?
+    other_user = User.first(username: username)
+    !other_user.nil? && other_user.password == password
+  end
 end
