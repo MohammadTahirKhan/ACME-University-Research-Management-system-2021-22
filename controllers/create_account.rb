f31487bf382b258f8d14ba0ed189f303e3f59e64
M Tahir Khan <mtkhan1@sheffield.ac.uk>
2022-03-10 14:27:00 +0000

    fixing minor problems in create account

--- controllers/create_account.rb
+++ controllers/create_account.rb
@@ -8,25 +8,15 @@ post "/create_account" do
   @institution_field = params["institution"]
   @password_field = params["password"]
 
-  @form_was_submitted = !@username_field.empty? && !@email_field.empty? && !@institution_field.empty? && !@password_field.empty? 
-  # && username_validation(@username_field)
+  @form_was_submitted = !@username_field.empty? && !@email_field.empty? && !@institution_field.empty? && !@password_field.empty?  && (DB[:users].where(username: @username_field)).count == 0
   @submission_error = nil
-
+  @username_validation_error = nil
   @username_error = nil
   @email_error = nil
   @institution_error = nil
   @password_error = nil
 
   @users.load(params)
-
-  # def username_validation(params["username"])
-  #   if (SELECT * FROM @users WHERE username == @username_field )
-  #     @username_validation_error = "Sorry, This username is not available"
-  #     return false 
-  #   end
-  #   return true
-  # end
-
 
   if @form_was_submitted
     # sanitise the values by removing whitespace
@@ -40,7 +30,7 @@ post "/create_account" do
   else
     # now proceed to validation
     @username_error = "Please enter a value for username" if @username_field.empty?
-    # @username_validation_error = "Sorry, This username is not available" if username_validation(@username_field)
+    @username_validation_error = "Sorry, This username is not available" if (DB[:users].where(username: @username_field)).count >0
     @email_error = "Please enter a value for email" if @email_field.empty?
     @institution_error = "Please enter a value for institution" if @institution_field.empty?
     @password_error = "Please enter a value for password" if @password_field.empty?
