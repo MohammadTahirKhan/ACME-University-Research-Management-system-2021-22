12367e84d0e84ddbede995a90ebb563ac62744eb
M Tahir Khan <mtkhan1@sheffield.ac.uk>
2022-03-14 22:27:50 +0000

    setting the default user_type poster when signing up

--- controllers/create_account.rb
+++ controllers/create_account.rb
@@ -5,9 +5,6 @@ post "/create_account" do
 
   @username_field = params["username"]
   @email_field = params["email"]
-  # if DB[:users].where(user_type: nil)
-  #   @user_type_field = "Poster"
-  # end
   @institution_field = params["institution"]
   @password_field = params["password"]
 
@@ -21,14 +18,12 @@ post "/create_account" do
 
   @users.load(params)
 
-  if @form_was_submitted
+  if @form_was_submitted 
     # sanitise the values by removing whitespace
     @username_field.strip!
     @email_field.strip!
     @institution_field.strip!
     @password_field.strip!
-    # @user_type_field.strip!
-
     @users.save_changes
     redirect "/login"
   else
