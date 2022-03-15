fcdfb1c0f0aee94bb55533735d9b79450ab9ea16
Rawan Al-Hashmi <raalhashmi1@sheffield.ac.uk>
2022-03-15 23:10:54 +0000

    fixed input type for password in create account page

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require "require_all"
 enable :sessions
 set :session_secret, "$g]Rd2M/WbJ`~~<GZWdH@Fm'ESk2_gckCtLJJkySYG"
 
+set :bind, '0.0.0.0'
+
 include ERB::Util
 require_rel "db/db", "models", "controllers"
 
