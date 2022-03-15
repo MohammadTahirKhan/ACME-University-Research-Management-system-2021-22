3b10faff0a50cb81988e05ab40752858a7a2066a
Jude Gibson <jsfgibson1@sheffield.ac.uk>
2022-03-15 13:00:39 +0000

    Login Sessions Work, however they cause tests to fail. Working on a solution

--- controllers/admin.rb
+++ controllers/admin.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 
 get "/admin" do
+  redirect "/login" unless session[:logged_in]
   @users = User.all
   erb :admin
 end
--- spec/end_to_end_test/create_post.rb
+++ spec/end_to_end_test/create_post.rb
@@ -2,6 +2,7 @@ require_relative "../spec_helper"
 
 describe "Create Post Page" do
   it "checks if a post is created" do
+    session[:logged_in] = true
     visit "/create_post"
     fill_in "title", with: "Test26_Title"
     fill_in "topic", with: "Test26_Topic"
@@ -15,6 +16,7 @@ describe "Create Post Page" do
     expect(page).to have_content "Test26_Institution"
     expect(page).to have_content "Test26_Link"
     DB.from("posts").delete
+    session.clear
   end
 
   it "will not create an account with no details" do
@@ -61,5 +63,35 @@ describe "Create Post Page" do
     click_button "Submit"
 
     expect(page).to have_content "Please enter a value for link"
+  end
+
+  it "can be accessed from the home page" do
+    visit "/home"
+    click_link "Post"
+    expect(page).to have_content "Create a Post"
+  end
+
+  it "can be accessed from the admin page" do
+    visit "/admin"
+    click_link "Post"
+    expect(page).to have_content "Create a Post"
+  end
+
+  it "can be accessed from the moderator page" do
+    visit "/moderator"
+    click_link "Post"
+    expect(page).to have_content "Create a Post"
+  end
+
+  it "can be accessed from the viewer page" do
+    visit "/viewer"
+    click_link "Post"
+    expect(page).to have_content "Create a Post"
+  end
+
+  it "can be accessed from the profile page" do
+    visit "/profile"
+    click_link "Post"
+    expect(page).to have_content "Create a Post"
   end
 end
