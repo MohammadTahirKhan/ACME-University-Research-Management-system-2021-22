f8d5746f1c8eb0a8abbf01aaf9e8d44498b630a6
Danial Asad <dasad1@sheffield.ac.uk>
2022-03-15 13:00:48 +0000

    Unit tests for create post page

---
+++ spec/unit_tests/viewer.rb
@@ -0,0 +1,49 @@
+require "rspec"
+require "rack/test"
+require_relative "../spec_helper"
+
+require_relative "../../app"
+
+RSpec.describe "Viewer page" do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  describe "Displays the posts table" do
+    it "displays a table when the database contains a post" do
+      get "/viewer"
+      if ( Post.all.count == 0) 
+        expect(last_response.body).to include("The database is empty!")
+      else 
+        expect(last_response.body).to include("Title")
+
+      end
+    end
+
+    describe "Displays the users table" do
+        it "displays a table when the database contains a user" do
+          get "/admin"
+          if ( User.all.count == 0) 
+            expect(last_response.body).to include("The database is empty!")
+          else 
+            expect(last_response.body).to include("Username")
+    
+          end
+        end
+#  Coverage tests
+    it "rejects the form when only title field is filled out" do
+      get "/create_post", "title" => "Some Text"
+     expect(last_response.body).to include("Please")
+     
+    end
+
+    it "rejects the form when only institution field is filled out" do
+      get "/create_post", "institution" => "Some Text"
+      expect(last_response.body).to include("Please")
+     
+    end
+  
+end
+end
