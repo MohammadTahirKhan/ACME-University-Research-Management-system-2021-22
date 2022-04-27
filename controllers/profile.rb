4bc9f80fcaed8cf7932bdfd29df0539696f9762b
M Tahir Khan <mtkhan1@sheffield.ac.uk>
2022-04-27 12:35:18 +0000

    view personal posts in the profile page

--- controllers/profile.rb
+++ controllers/profile.rb
@@ -8,6 +8,15 @@ get "/profile" do
   else
     User.where(Sequel.like(:username, "%#{@user_search}%"))
   end
+
+  @post_search = params.fetch("post_search", "").strip
+
+  # shows the result according to the search bar 
+  @posts = if @post_search.empty?
+    Post.all
+  else
+    Post.where(Sequel.like(:poster_name, "%#{@post_search}%"))
+  end
   
   erb :profile
 end
